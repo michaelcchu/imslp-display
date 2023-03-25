ea654cb8950c8bfc9f3e6e700704896a8dd910c8
michaelcchu <michael02chu@gmail.com>
2023-03-24 22:49:51 -0400

    show all pages of pdf on the html page

--- index.js
+++ index.js
@@ -1,41 +1,38 @@
-const canvas = document.getElementById('layer1');
-const context = canvas.getContext('2d', {willReadFrequently: true});
-
 var loadingTask = pdfjsLib.getDocument('IMSLP23765-PMLP01595-Beethoven_Symphony6_Cls.pdf');
 loadingTask.promise.then(function(pdf) {
-  // you can now use *pdf* here
-  pdf.getPage(1).then(function(page) {
-    // you can now use *page* here
-    var scale = 1;
-    var viewport = page.getViewport({ scale: scale, });
-    // Support HiDPI-screens.
-    var outputScale = window.devicePixelRatio || 1;
+    const numPages = pdf.numPages;
+    for (let i = 0; i < numPages; i++) {
+        // you can now use *pdf* here
+        pdf.getPage(i+1).then(function(page) {
+            // you can now use *page* here
+            var scale = 1;
+            var viewport = page.getViewport({ scale: scale, });
+            // Support HiDPI-screens.
+            var outputScale = window.devicePixelRatio || 1;
 
-    canvas.width = Math.floor(viewport.width * outputScale);
-    canvas.height = Math.floor(viewport.height * outputScale);
+            const canvas = document.createElement("canvas");
+            const context = canvas.getContext('2d', {willReadFrequently: true});
 
-    var transform = outputScale !== 1
-    ? [outputScale, 0, 0, outputScale, 0, 0]
-    : null;
+            document.getElementById("canvases").appendChild(canvas);
 
-    var renderContext = {
-        canvasContext: context,
-        transform: transform,
-        viewport: viewport
-    };
+            canvas.width = Math.floor(viewport.width * outputScale);
+            canvas.height = Math.floor(viewport.height * outputScale);
 
-    var renderingTask = page.render(renderContext);
+            var transform = outputScale !== 1
+            ? [outputScale, 0, 0, outputScale, 0, 0]
+            : null;
 
-    renderingTask.promise.then(function() {
-        //initializeCanvases(); 
-    });
-  });
-});
+            var renderContext = {
+                canvasContext: context,
+                transform: transform,
+                viewport: viewport
+            };
 
-function initializeCanvases() {
-    for (let i=2; i<=4; i++) {
-        const canvas = document.getElementById('layer'+i);
-        canvas.width = document.getElementById('layer1').width;
-        canvas.height = document.getElementById('layer1').height;
+            var renderingTask = page.render(renderContext);
+
+            renderingTask.promise.then(function() {
+                // do drawing/graphics/image-processing/computer-vision stuff 
+            });
+        });
     }
-}
+});
